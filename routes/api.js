a8ea6c217a52fc505d5eab1b2eec84c8afc5518e
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-12-09 15:33:26 +0000

    Added module routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { programmeData, autocompleteData } = require('../controllers/programmes');
+const { moduleData, moduleAutocompleteData } = require('../controllers/modules');
 
 router.get('/data/:progCode/:cohort/:year', programmeData);
 router.get('/autocomplete-data', autocompleteData)
+router.get('/mod-data/:modCode/:year', moduleData);
+router.get('/mod-autocomplete-data', moduleAutocompleteData)
 
 module.exports = router;
