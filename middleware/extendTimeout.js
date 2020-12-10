3cfb81209b62a9acc46f387c63b2f3e7ced83306
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-12-10 23:17:26 +0000

    Change to middleware

--- middleware/extendTimeout.js
+++ middleware/extendTimeout.js
@@ -6,7 +6,7 @@ const extendTimeoutMiddleware = (req, res, next) => {
   let isDataSent = false;
 
   // Only extend the timeout for API requests
-  if (!req.url.includes('/api')) {
+  if (!req.url.includes('/mod-autocomplete-data')) {
     next();
     return;
   }
