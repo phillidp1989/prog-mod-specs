8a5063a811e637ec4faf6efa0991026b868280be
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-12-10 23:11:36 +0000

    Middleware to extend timeout

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { programmeData, autocompleteData } = require('../controllers/programmes');
 const { moduleData, moduleAutocompleteData } = require('../controllers/modules');
+const extendTimeoutMiddleware = require("../middleware/extendTimeout");
 
 router.get('/data/:progCode/:cohort/:year', programmeData);
 router.get('/autocomplete-data', autocompleteData)
 router.get('/mod-data/:modCode/:year', moduleData);
-router.get('/mod-autocomplete-data', moduleAutocompleteData)
+router.get('/mod-autocomplete-data', extendTimeoutMiddleware, moduleAutocompleteData)
 
 module.exports = router;
